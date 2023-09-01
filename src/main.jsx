bb3326e75fe8d28ddcc5afebf718d0e155b8f6ab
AbdallaMon <abdotlos60@gmail.com>
2023-09-01 17:09:59 +0200

    first

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// import "./scss/app.css";
+import { HashRouter } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import "normalize.css"; // Note this
+library.add(fas, far, fab);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
