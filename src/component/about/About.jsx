df2d2e4238b3c5a25d318236d971ffebd0265a05
AbdallaMon <abdotlos60@gmail.com>
2023-09-05 17:07:05 +0200

    finist about

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -12,6 +12,7 @@ export default function About() {
       initial={{ scale: 0 }}
       animate={animation ? { x: "100vw" } : { scale: [0, 1] }}
       transition={{ duration: 0.5 }}
+      className="about"
     >
       <div className="about-landing">
         <div className="about-container">
@@ -24,7 +25,82 @@ export default function About() {
           <Heading value={arr3} head={"Developer"} />
         </div>
       </div>
+      <AboutText />
     </motion.section>
+  );
+}
+function AboutText() {
+  return (
+    <div className="about-text">
+      <div className="container mx-auto">
+        <div className="about-me">
+          <div className="about-box left">
+            <AboutBox
+              text={
+                <>
+                  <p>
+                    I&apos;m a Frontend Web Developer building the Front-end of
+                    Websites and Web Applications that leads to the success of
+                    the overall product. Check out some of my work in the
+                    Projects section.
+                  </p>
+                  <p>
+                    I&apos;m open to Job opportunities where I can contribute,
+                    learn and grow. If you have a good opportunity that matches
+                    my skills then don&apos;t hesitate to contact me.
+                  </p>
+                </>
+              }
+              heading={"About me"}
+            />
+          </div>
+          <div className="about-box right ">
+            <AboutBox text={<Skills />} heading="Skills" />
+          </div>
+          <div className="about-box left">
+            <AboutBox text={<Education />} heading="Education" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function AboutBox({ classN, text, heading }) {
+  return (
+    <>
+      <div>{text}</div>
+      <h2>{heading}</h2>
+    </>
+  );
+}
+function Skills() {
+  let skills = [
+    "HTML",
+    "CSS",
+    "Scss",
+    "Bootstrap",
+    "Tailwind CSS",
+    "MUI",
+    "Javascript",
+    "React",
+    "React Redux",
+  ];
+  return (
+    <>
+      {skills.map((e) => (
+        <div className="skill">{e}</div>
+      ))}
+    </>
+  );
+}
+function Education() {
+  return (
+    <>
+      <h4>Bachelor of Nursing</h4>
+      <h5>2020 - 2025</h5>
+      <p>I&apos;m a student in faculty of nursing cairo university.</p>
+    </>
   );
 }
 function Heading({ head, text, value }) {
@@ -38,7 +114,7 @@ function Heading({ head, text, value }) {
           <p>
             Hello Iam
             <span>Abdalla Abdelsabour</span> a frontend developer currently
-            available to work as freelancer or full-time. `
+            available to work as freelancer or full-time.
           </p>
         </div>
       ) : null}
